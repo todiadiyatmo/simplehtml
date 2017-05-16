f72b91a92558df08d153c118de6bb4e2c26149d0
todi adiyatmo wijoyo <t@tonjoo.id>
2017-05-16 22:16:59 +0700

    final

--- block_simplehtml.php
+++ block_simplehtml.php
@@ -1,5 +1,6 @@
 <?php
 class block_simplehtml extends block_base {
+	
     public function init() {
         $this->title = get_string('simplehtml', 'block_simplehtml');
     }
@@ -7,35 +8,35 @@ class block_simplehtml extends block_base {
     // The PHP tag and the curly bracket for the class definition 
     // will only be closed after there is another function added in the next section.
     public function get_content() {
+ 	   
  	   if ($this->content !== null) {
      	 return $this->content;
-    }
+    	}
  
-    $this->content         =  new stdClass;
+    	$this->content =  new stdClass;
     
-    ob_start();
+    	ob_start();
 
-    ?>
+    	?>
 
-    <h1>Ini adalah block</h1>
+	    <h1>Ini adalah block</h1>
 
-    <span>
-    <?php
-    if (! empty($this->config->text)) {   
-    	echo $this->config->text;  
-	}
-	?>
-	</span>
+	    <span>
+	    <?php
+	    if (! empty($this->config->text)) {   
+	    	echo $this->config->text;  
+		}
+		?>
+		</span>
 
-    <?php
+	    <?php
+		$output = ob_get_contents();
+		ob_end_clean();
+
+		$this->content->text   = $output;
+	    $this->content->footer = 'Footer here...';
 	 
-	$output = ob_get_contents();
-	ob_end_clean();
-
-	$this->content->text   = $output;
-    $this->content->footer = 'Footer here...';
- 
-    return $this->content;
+	    return $this->content;
 	}
 	
 }
