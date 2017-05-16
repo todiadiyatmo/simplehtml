42447dca41af672cc50bb24ae4bfae12efca00aa
todi adiyatmo wijoyo <t@tonjoo.id>
2017-05-16 22:29:26 +0700

    rdf

--- block_simplehtml.php
+++ block_simplehtml.php
@@ -9,6 +9,8 @@ class block_simplehtml extends block_base {
     // will only be closed after there is another function added in the next section.
     public function get_content() {
  	   
+       include "easyrdf/vendor/autoload.php";
+
  	   if ($this->content !== null) {
      	 return $this->content;
     	}
@@ -26,6 +28,12 @@ class block_simplehtml extends block_base {
 	    if (! empty($this->config->text)) {   
 	    	echo $this->config->text;  
 		}
+		
+		// RDF Loaded here
+		$foaf =  \EasyRdf\Graph::newAndLoad('http://njh.me/foaf.rdf');
+		$me = $foaf->primaryTopic();
+		echo "My name is: ".$me->get('foaf:name')."\n";
+
 		?>
 		</span>
 
